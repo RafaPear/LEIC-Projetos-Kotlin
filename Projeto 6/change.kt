dcb72c72aa48be1e46d78225b6ff008c31fcb55a
RafaPear <rafapear@outlook.pt>
2024-10-04 19:54:18 +0100

    updated change.kt code to acount for 1 cent

--- Projeto 6/change.kt
+++ Projeto 6/change.kt
@@ -4,20 +4,20 @@ fun main(){
 	val price : Double = readln().toDouble()
 	print("Input given amount: ")
 	val money : Double = readln().toDouble()
-	var change : Double = money - price
+	val change : Double = money - price
 
-	var euro2  : Int = (change/2.0).toInt()
-    var euro1  : Int = ((change%2.0)/1.0).toInt()
-    var cent50 : Int = (((change%2.0)%1.0)/0.5).toInt()
-    var cent20 : Int = ((((change%2.0)%1.0)%0.5)/0.2).toInt()
-    var cent10 : Int = (((((change%2.0)%1.0)%0.5)%0.2)/0.1).toInt()
-    var cent5  : Int = ((((((change%2.0)%1.0)%0.5)%0.2)%0.1)/0.05).toInt()
-    var cent2  : Int = (((((((change%2.0)%1.0)%0.5)%0.2)%0.1)%0.05)/0.02).toInt()
+	val euro2  : Int = (change/2.0).toInt()
+    val euro1  : Int = ((change%2.0)/1.0).toInt()
+    val cent50 : Int = (((change%2.0)%1.0)/0.5).toInt()
+    val cent20 : Int = ((((change%2.0)%1.0)%0.5)/0.2).toInt()
+    val cent10 : Int = (((((change%2.0)%1.0)%0.5)%0.2)/0.1).toInt()
+    val cent5  : Int = ((((((change%2.0)%1.0)%0.5)%0.2)%0.1)/0.05).toInt()
+    val cent2  : Int = (((((((change%2.0)%1.0)%0.5)%0.2)%0.1)%0.05)/0.02).toInt()
     var cent1  : Int = ((((((((change%2.0)%1.0)%0.5)%0.2)%0.1)%0.05)%0.02)/0.01).toInt()
 
     val n = euro2*2 + euro1*1 + cent50*0.5 + cent20*0.2 + cent10*0.1 + cent5*0.05 + cent2*0.02 + cent1*0.01
 
-    //if(n != change) cent1++
+    if(n != change) cent1++
 
     if (euro2 !=0)   print("$euro2 2 euro coins\n")
     if (euro1 != 0)  print("$euro1 1 euro coins\n")
